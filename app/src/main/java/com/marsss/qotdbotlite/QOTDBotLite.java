c3da121165009410a45c7523bcc4c478687221a8
itsmarsss <itsmarzzzzzz@gmail.com>
2023-06-30 22:35:41 -0400

    Update template

--- app/src/main/java/com/marsss/qotdbotlite/QOTDBotLite.java
+++ app/src/main/java/com/marsss/qotdbotlite/QOTDBotLite.java
@@ -33,46 +33,25 @@ public class QOTDBotLite {
     private static boolean autostart = false;
 
     private final static String template = """
-            # IMPORTANT - Mandatory fields:\r
-            # Input the bot's token here, this can be found in Developers Portal > Applications > [Bot Profile] > Bot > Token > [Copy]\r
-            botToken: %s\r
-            \r
-            # Turn on developers mode in Settings > Advanced > Developer Mode, right click your Discord Server and click on [Copy ID]\r
-            serverID: %s\r
-            \r
-            # Right click your QOTD channel and click on [Copy ID]\r
-            channelID: %s\r
-            \r
-            # Set a prefix here for the bot, this is what members use to use the bot (e.g. qotd help)\r
-            prefix: %s\r
-            \r
-            # Input number of minute(s) until another QOTD is sent ( 1 to 1440 [24 hours] )\r
-            interval: %s\r
-            \r
-            # Start time, this dictates what time of the day your QOTD will be sent (24 hours time, local server time)\r
-            # Hours\r
-            hour: %s\r
-            # Minutes\r
-            minute: %s\r
-            \r
-            # Not mandatory fields:\r
-            # Set a perm role, these members can add QOTDs (write everyone if everyone)\r
-            permRoleID: %s\r
-            \r
-            # Set a manager role, these members can manage QOTDs (write everyone if everyone)\r
-            managerRoleID: %s\r
-            \r
-            # Dynamic config.yml, config.yml is changed as its values are changed\r
-            dynamicConfig: %s\r
-            \r
-            # QOTD submission review settings\r
-            # Set to true if you want QOTD submissions to go through bot manager review\r
-            managerReview: %s\r
-            # Right click your QOTD review channel and click on [Copy ID], this is where QOTD submissions are reviewed\r
-            reviewChannel: %s\r
-            \r
-            # QOTD Embed color in hex (Do not include "#")\r
-            QOTDColor: %s\r
+        # IMPORTANT - Mandatory fields:\r
+        # Input the bot's token here, this can be found in Developers Portal > Applications > [Bot Profile] > Bot > Token > [Copy]\r
+        botToken: %s\r
+        \r
+        # Turn on developers mode in Settings > Advanced > Developer Mode, right-click your Discord Server and click on [Copy ID]\r
+        serverID: %s\r
+        \r
+        # Right-click your QOTD channel and click on [Copy ID]\r
+        channelID: %s\r
+        \r
+        # Set a prefix here for the bot, this is what members use to use the bot (e.g. qotd help)\r
+        prefix: %s\r
+        \r
+        # Not mandatory fields:\r
+        # Set a manager role, these members can manage QOTDs (write everyone if everyone)\r
+        managerRoleID: %s\r
+        \r
+        # QOTD Embed color in hex (Do not include "#")\r
+        QOTDColor: %s\r
             """;
 
     public static void main(String[] args) throws URISyntaxException {
